e55e9d5a24deb31a63d864699d80e461aabe7f2b
hvenanc <hvs.git@gmail.com>
2025-01-02 20:29:22 -0300

    Envio das Respostas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+
+      <template v-for="answer in this.answers" v-bind:key="answer">
+        <input 
+        type="radio" 
+        name="options" 
+        id="answer"
+        :value="answer"
+        v-model="this.chosenAnswer" />
+        <label v-html="answer"></label><br />
+      </template>
+      <button type="submit" @click="this.submitAwswer()">Check</button>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined
+    };
+  },
+
+  computed: {
+    answers() {
+      let answers = [...this.incorrectAnswers];
+      let randowNumber = Math.round(Math.random() * answers.length);
+      answers.splice(randowNumber, 0, this.correctAnswer);
+      return answers;
+    },
+  },
+
+  created() {
+    const api =
+      "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy";
+    this.axios.get(api).then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+    });
+  },
+  methods: {
+    submitAwswer() {
+      if(!this.chosenAnswer) {
+        alert("Select one answer");
+      }
+      else {
+        if(this.chosenAnswer == this.correctAnswer) {
+          alert("Correct")
+        }
+        else {
+          alert("You lose!")
+        }
+      }
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
